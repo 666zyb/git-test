21ec1ec9a65354fe82f688753922ff15b94e97dd
666zyb <1992002335@qq.com>
2024-11-27 15:19:33 +0800

    add Person.java

---
+++ Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+    public static void main(String[] args){
+        String name="张三";
+        String sex="男";
+        int age=16;
+        float height=170;
+        float weight=70;
+        String address="山西省太原市山西大学";
+        System.out.println("姓名:"+name+"\n"+"性别:"+sex+"\n"+"年龄:"+age+"\n"+"身高:"+height+"\n"+"体重:"+weight+"\n"+"地址:"+address);
+    }
+}
